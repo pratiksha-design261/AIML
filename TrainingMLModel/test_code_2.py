b390418a1ad947e36a4366150237bf2609d41bd3
pratiksha-design261 <66290960+pratiksha-design261@users.noreply.github.com>
2026-01-08 00:42:16 +0530

    Update test_code_2.py

--- TrainingMLModel/test_code_2.py
+++ TrainingMLModel/test_code_2.py
@@ -20,7 +20,7 @@ X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_
 # standardize the feature 
 scaler = StandardScaler()
 X_train_scale = scaler.fit_transform(X_train)
-X_test_scale = scaler.transform(X_test))
+X_test_scale = scaler.transform(X_test)
 
 # create a ml model
 model = LogisticRegression()
